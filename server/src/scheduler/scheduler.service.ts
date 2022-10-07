e3078e954f266669f016a0e1b22a308c4fae62a9
jjcc1421 <jjcc1421@gmail.com>
2022-10-07 14:57:25 -0500

    Updated scheduler

--- server/src/scheduler/scheduler.service.ts
+++ server/src/scheduler/scheduler.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
 import { InjectRepository } from "@nestjs/typeorm";
+import { HardwareService } from "src/hardware/hardware.service";
 import { Repository } from "typeorm";
 import { Scheduler } from "./scheduler.entity";
 
@@ -8,6 +10,7 @@ export class SchedulerService {
   constructor(
     @InjectRepository(Scheduler)
     private schedulerRepository: Repository<Scheduler>,
+    private hardwareService: HardwareService,
   ) {}
 
   async createScheduler() {
@@ -30,7 +33,40 @@ export class SchedulerService {
     // return this.schedulerRepository.findOne();
   }
 
+  private async getOnScheduler() {
+    const schedulers = await this.schedulerRepository.find({ 
+      where: { enabled: true } 
+    });
+
+    return schedulers.map(scheduler => {
+      const startDate = new Date();
+      const endDate = new Date();
+      const [startTimeHour, startTimeMinute] = scheduler.startHour.split(':');
+      const [endTimeHour, endTimeMinute] = scheduler.endHour.split(':');
+      startDate.setHours(parseInt(startTimeHour), parseInt(startTimeMinute));
+      endDate.setHours(parseInt(endTimeHour), parseInt(endTimeMinute));
+      return {
+        ...scheduler,
+        startDate: startDate,
+        endDate: endDate,
+      }    
+    }).filter(scheduler => {
+      const now = new Date();
+      return now >= scheduler.startDate && now <= scheduler.endDate;
+    })
+  
+  }
+
+  @Cron('60 * * * * *')
   async executeScheduler() {
     // TODO: implement this
+    const onSchedulers = await this.getOnScheduler()
+    if(onSchedulers.length > 0) {
+      await this.hardwareService.turnOn();
+    }
+    else {
+      await this.hardwareService.turnOff();
+    }
   }
+  
 }
