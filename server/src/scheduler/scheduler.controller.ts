5548c43ebaef9d1f3fd2d20073291ac84f354f0f
Anderson LM <anderson1564@gmail.com>
2022-10-07 14:55:32 -0500

    Add CRUDL for scheduler

--- server/src/scheduler/scheduler.controller.ts
+++ server/src/scheduler/scheduler.controller.ts
@@ -1,28 +1,53 @@
-import { Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { SchedulerService } from './scheduler.service';
 
 @Controller('scheduler')
 export class SchedulerController {
+  constructor(private readonly schedulerService: SchedulerService) {}
+
   @Get(':id')
-  async getScheduler(
-    @Param('id') id: string,
-  ) {
-    return id;
-  }
-
-  @Post()
-  async createScheduler() {
-    return 'create';
-  }
-
-  @Delete(':id')
-  async deleteScheduler(
-    @Param('id') id: string,
-  ) {
-    return 'delete'
+  async getScheduler(@Param('id') id: number) {
+    const scheduler = await this.schedulerService.getSchedulerById(id);
+    return scheduler;
   }
 
   @Get()
   async getAllSchedulers() {
-    return 'getAll';
+    const schedulers = await this.schedulerService.getAllSchedulers();
+    return schedulers;
+  }
+
+  @Patch(':id')
+  async updateScheduler(
+    @Param('id') id: number,
+    @Body('startHour') startHour?: string,
+    @Body('endHour') endHour?: string,
+    @Body('enabled') enabled?: boolean
+  ) {
+    const scheduler = await this.schedulerService.updateScheduler({
+      id,
+      startHour,
+      endHour,
+      enabled,
+    });
+    return scheduler;
+  }
+
+  @Post()
+  async createScheduler(
+    @Body('startHour') startHour: string,
+    @Body('endHour') endHour: string,
+  ) {
+    const scheduler = await this.schedulerService.createScheduler({
+      startHour,
+      endHour,
+    });
+    return scheduler;
+  }
+
+  @Delete(':id')
+  async deleteScheduler(@Param('id') id: number) {
+    const scheduler = await this.schedulerService.deleteScheduler(id);
+    return scheduler;
   }
 }
--- server/src/scheduler/scheduler.service.ts
+++ server/src/scheduler/scheduler.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Scheduler } from "./scheduler.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Scheduler } from './scheduler.entity';
 
 @Injectable()
 export class SchedulerService {
@@ -10,24 +10,50 @@ export class SchedulerService {
     private schedulerRepository: Repository<Scheduler>,
   ) {}
 
-  async createScheduler() {
-    // return this.schedulerRepository.save();
+  async createScheduler({
+    startHour,
+    endHour,
+    enabled,
+  }: {
+    startHour: string;
+    endHour: string;
+    enabled?: boolean;
+  }) {
+    return await this.schedulerRepository.save({
+      startHour,
+      endHour,
+      enabled,
+    });
   }
 
-  async deleteScheduler() {
-    // return this.schedulerRepository.delete();
+  async deleteScheduler(id: number) {
+    return await this.schedulerRepository.delete(id);
   }
 
   async getAllSchedulers() {
-    // return this.schedulerRepository.find();
+    return await this.schedulerRepository.find();
   }
 
-  async updateScheduler() {
-    // return this.schedulerRepository.update();
+  async updateScheduler({
+    id,
+    startHour,
+    endHour,
+    enabled,
+  }: {
+    id: number;
+    startHour?: string;
+    endHour?: string;
+    enabled?: boolean;
+  }) {
+    return await this.schedulerRepository.update(id, {
+      startHour,
+      endHour,
+      enabled,
+    });
   }
 
-  async getSchedulerById() {
-    // return this.schedulerRepository.findOne();
+  async getSchedulerById(id: number) {
+    return await this.schedulerRepository.findOneBy({ id });
   }
 
   async executeScheduler() {
