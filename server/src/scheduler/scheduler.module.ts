f2456c1ced1e2b605f204cb2db7b3ebdee2d2b7f
jjcc1421 <jjcc1421@gmail.com>
2022-10-07 15:05:57 -0500

    Updated nest deps

--- server/src/scheduler/scheduler.module.ts
+++ server/src/scheduler/scheduler.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { HardwareService } from "src/hardware/hardware.service";
 import { SchedulerController } from "./scheduler.controller";
 import { Scheduler } from "./scheduler.entity";
 import { SchedulerService } from "./scheduler.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Scheduler])],
+  imports: [TypeOrmModule.forFeature([Scheduler]), HardwareService],
   controllers: [SchedulerController],
-  providers: [SchedulerService],
+  providers: [SchedulerService, HardwareService],
 })
 export class SchedulerModule {}
